07cadd0012ed4a51c0f446a47a7f86396a77217f
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-04-15 11:58:08 +0600

    circle added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # android_IntroToCompose
+
+// 008 Introduction to MutableState - Incrementing a Counter -- 00:08:08
--- app/src/main/java/com/axiagroups/introtocompose/MainActivity.kt
+++ app/src/main/java/com/axiagroups/introtocompose/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.axiagroups.introtocompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardDefaults.cardElevation
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.axiagroups.introtocompose.ui.theme.IntroToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +38,66 @@ class MainActivity : ComponentActivity() {
         setContent {
             IntroToComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Myapp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Myapp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.primary,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "$100",
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 39.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            Spacer(modifier = Modifier.height(130.dp))
+            CreateCircle()
+        }
+
+    }
 }
+
+@Preview
+@Composable
+fun CreateCircle() {
+    var moneyCounter = 0
+    Card(
+        modifier = Modifier
+            .padding(3.dp)
+            .size(145.dp)
+            .clickable {
+                moneyCounter += 100
+            },
+        shape = CircleShape,
+        elevation = cardElevation(4.dp),
+    ) {
+        Box(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Tap")
+        }
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IntroToComposeTheme {
-        Greeting("Android")
+        Myapp()
     }
 }
